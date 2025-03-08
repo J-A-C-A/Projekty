2c8eafc8210388f7e29d5eaa89a3e2bac2a6c270
Jan Konczal <163431600+J-A-C-A@users.noreply.github.com>
2025-03-08 14:18:37 +0100

    Delete Snake_2 directory

--- Snake_2/src/Constants.java
+++
@@ -1,6 +0,0 @@
-public class Constants { //ustawiamy tutać jak ma wyglądać okno rozgrywki plus 3 linijka daje nam tytuł gry na ekranie. Możemy tutaj pozmieniać niektóre rzeczy lub dodać do wyglądu ekranu gry
-	protected static final int SCREEN_WIDTH = 800;
-	protected static final int SCREEN_HEIGHT = 600;
-	protected static final String SCREEN_TITLE = "Snake";
-	protected static final float TILE_WIDTH = 24;
-}
